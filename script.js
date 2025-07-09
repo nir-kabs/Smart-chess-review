38ec02ed414b76d35fae6b3e31f8392ba2cd6bc9
nir-kabs <kabessanir@gmail.com>
2025-07-09 14:41:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const backgroundSelect = document.getElementById('backgroundSelect');
 const themeSelect = document.getElementById('themeSelect');
 
 backgroundSelect.addEventListener('change', () => {
-  document.body.className = ''; // Сбросить все классы
+  document.body.className = '';
   document.body.classList.add(backgroundSelect.value);
 });
 
 themeSelect.addEventListener('change', () => {
   console.log('Selected chessboard theme:', themeSelect.value);
-  // Здесь можно добавить код для смены темы доски
+  // Добавить код смены темы доски здесь
 });
