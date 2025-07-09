937a95842d08a4079edb8788ea2bf8a340701c1a
nir-kabs <kabessanir@gmail.com>
2025-07-09 13:13:26 +0300

    Update script.js 1

--- script.js
+++ script.js
@@ -1,34 +1,12 @@
-const boardElement = document.getElementById('board');
+// Theme selector for chessboard (placeholder)
 const themeSelect = document.getElementById('themeSelect');
-
-const boardThemes = {
-  classic: 'https://chessboardjs.com/img/chesspieces/wikipedia/{piece}.png',
-  modern: 'https://images.chesscomfiles.com/chess-themes/pieces/neo/150/{piece}.png'
-};
-
-let currentTheme = 'classic';
-
-const chess = new Chess();
-
-const config = {
-  draggable: true,
-  position: 'start',
-  pieceTheme: function(piece) {
-    return boardThemes[currentTheme].replace('{piece}', piece);
-  },
-  onDrop: function(source, target) {
-    const move = chess.move({ from: source, to: target, promotion: 'q' });
-    if (move === null) return 'snapback';
-  }
-};
-
-let board = Chessboard(boardElement, config);
-
 themeSelect.addEventListener('change', () => {
-  currentTheme = themeSelect.value;
-  board.destroy();
-  config.pieceTheme = function(piece) {
-    return boardThemes[currentTheme].replace('{piece}', piece);
-  };
-  board = Chessboard(boardElement, config);
+  console.log('Selected chessboard theme:', themeSelect.value);
+  // TODO: implement real theme switching for the board
+});
+
+// Background selector
+const backgroundSelect = document.getElementById('backgroundSelect');
+backgroundSelect.addEventListener('change', () => {
+  document.body.className = backgroundSelect.value;
 });
