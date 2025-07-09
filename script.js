b2beb44ec5c96ef742ee757ab64160f5e0b56fec
nir-kabs <kabessanir@gmail.com>
2025-07-09 14:04:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-// Background selector
 const backgroundSelect = document.getElementById('backgroundSelect');
 backgroundSelect.addEventListener('change', () => {
-  document.body.className = ''; // Remove any previous background class
-  document.body.classList.add(backgroundSelect.value); // Apply selected background class
+  document.body.className = '';
+  document.body.classList.add(backgroundSelect.value);
 });
 
-// Chessboard theme selector
 const themeSelect = document.getElementById('themeSelect');
 themeSelect.addEventListener('change', () => {
   console.log('Selected chessboard theme:', themeSelect.value);
-  // TODO: Apply theme to chessboard when it's implemented
 });
